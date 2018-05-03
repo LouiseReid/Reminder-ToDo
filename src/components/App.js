42195e2f42fe43d05a79a02e5f667c8c2605e865
Louise Reid <l_reid@hotmail.co.uk>
2018-05-03 10:27:29 +0100

    app using cookies

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import { addReminder, deleteReminder } from '../actions';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      dueDate: ''
     }
   }
 
   addReminder() {
-    this.props.addReminder(this.state.text);
+    this.props.addReminder(this.state.text, this.state.dueDate);
   }
 
   deleteReminder(id){
@@ -26,48 +28,56 @@ class App extends React.Component {
           reminders.map(reminder => {
             return(
               <li key={reminder.id} className="list-group-item">
-                <div className="list-item">{reminder.text}</div>
+                <div className="list-item">
+                  <div>{reminder.text}</div>
+                  <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+                </div>
                 <div className="list-item delete-button"
                   onClick={() => this.deleteReminder(reminder.id)}>&#x2715;</div>
-              </li>
-            )
-          })
-        }
-      </ul>
-    )
-  }
+                </li>
+              )
+            })
+          }
+        </ul>
+      )
+    }
 
-  render(){
-    return (
-      <div className="App">
-        <div className="title">
-          Reminder Pro
-        </div>
-        <div className="form-inline reminder-form">
-          <div className="form-group">
-            <input
-              className="form-control"
-              placeholder="I have to..."
-              onChange={event => this.setState({text: event.target.value})}
-            />
+    render(){
+      return (
+        <div className="App">
+          <div className="title">
+            Reminder Pro
           </div>
-          <button
-            type="button"
-            className="btn btn-success"
-            onClick={() => this.addReminder()}
-            >
-              Add Reminder</button>
+          <div className="form-inline reminder-form">
+            <div className="form-group">
+              <input
+                className="form-control"
+                placeholder="I have to..."
+                onChange={event => this.setState({text: event.target.value})}
+              />
+              <input
+                className="form-control"
+                type="datetime-local"
+                onChange={event => this.setState({dueDate: event.target.value})}
+              />
             </div>
-            {this.renderReminders()}
-          </div>
-        )
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={() => this.addReminder()}
+              >
+                Add Reminder</button>
+              </div>
+              {this.renderReminders()}
+            </div>
+          )
+        }
       }
-    }
 
-    function mapStateToProps(state){
-      return {
-        reminders: state
+      function mapStateToProps(state){
+        return {
+          reminders: state
+        }
       }
-    }
 
-    export default connect(mapStateToProps, { addReminder, deleteReminder })(App);
+      export default connect(mapStateToProps, { addReminder, deleteReminder })(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
-import { ADD_REMINDER, DELETE_REMINDER } from '../constants'
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 const reminder = (action) => {
-  let { text } = action;
+  let { text, dueDate } = action;
   return {
+    id: Math.random(),
     text,
-    id: Math.random()
+    dueDate
   }
 }
 
@@ -16,13 +18,15 @@ const removeByID = (state = [], id) => {
 
 const reminders = (state = [], action) => {
   let reminders = null;
+  state = read_cookie('reminders');
   switch (action.type) {
     case ADD_REMINDER:
       reminders = [...state, reminder(action)];
-      console.log('reminders as state', reminders);
+      bake_cookie('reminders', reminders)
       return reminders;
     case DELETE_REMINDER:
       reminders = removeByID(state, action.id);
+      bake_cookie('reminders', reminders)
       return reminders;
     default:
       return state;
